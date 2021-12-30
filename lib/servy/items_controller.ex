6f0fb8ae9fd15a093a3d9e16de156fabf12116a1
LuizFerK <luizfernandoklein@live.com>
2021-12-30 14:19:09 -0300

    feat: HTML templates with comprehensions

--- lib/servy/items_controller.ex
+++ lib/servy/items_controller.ex
@@ -3,20 +3,29 @@ defmodule Servy.ItemsController do
   alias Servy.Items.GetAll, as: GetAllItems
   alias Servy.Items.Get, as: GetItem
 
+  @templates_path Path.expand("../../templates", __DIR__)
+
+  defp render(conn, template, bindings) do
+    content =
+      @templates_path
+      |> Path.join(template)
+      |> EEx.eval_file(bindings)
+
+    %{conn | status: 200, resp_body: content}
+  end
+
   def index(%Conn{} = conn) do
     items =
       GetAllItems.call()
       |> Enum.sort(fn i1, i2 -> i1.name <= i2.name end)
-      |> Enum.map(fn item -> "<li>#{item.name} - #{item.color}</li>" end)
-      |> Enum.join()
 
-    %{conn | status: 200, resp_body: "<ul>#{items}</ul>"}
+    render(conn, "index.eex", items: items)
   end
 
   def show(%Conn{} = conn, %{"id" => id}) do
     item = GetItem.call(id)
 
-    %{conn | status: 200, resp_body: "<h1>Item #{item.id}: #{item.name}</h1>"}
+    render(conn, "show.eex", item: item)
   end
 
   def create(%Conn{} = conn, %{"name" => name, "color" => color}) do
