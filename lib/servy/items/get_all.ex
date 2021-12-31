9a36262ad2c4878e4b152bb9bfd497346278fb10
LuizFerK <luizfernandoklein@live.com>
2021-12-31 01:17:36 -0300

    feat: Read items from JSON file

--- lib/servy/items/get_all.ex
+++ lib/servy/items/get_all.ex
@@ -1,18 +1,24 @@
 defmodule Servy.Items.GetAll do
   alias Servy.Item
 
+  @json_path Path.expand("../../../priv/json", __DIR__)
+
   def call do
-    [
-      %Item{id: 1, name: "Item 1", color: "red", in_stock: true},
-      %Item{id: 2, name: "Item 2", color: "yellow", in_stock: true},
-      %Item{id: 3, name: "Item 3", color: "green", in_stock: true},
-      %Item{id: 4, name: "Item 4", color: "black", in_stock: true},
-      %Item{id: 5, name: "Item 5", color: "white"},
-      %Item{id: 6, name: "Item 6", color: "pink"},
-      %Item{id: 7, name: "Item 7", color: "brown", in_stock: true},
-      %Item{id: 8, name: "Item 8", color: "gray", in_stock: true},
-      %Item{id: 9, name: "Item 9", color: "purple", in_stock: true},
-      %Item{id: 10, name: "Item 10", color: "blue", in_stock: true}
-    ]
+    @json_path
+    |> Path.join("items.json")
+    |> read_json
+    |> Poison.decode!(as: %{"items" => [%Item{}]})
+    |> Map.get("items")
+  end
+
+  defp read_json(source) do
+    case File.read(source) do
+      {:ok, contents} ->
+        contents
+
+      {:error, reason} ->
+        IO.inspect("Error reading #{source}: #{reason}")
+        "[]"
+    end
   end
 end
