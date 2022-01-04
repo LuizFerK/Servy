d504fc4525ed8c4c587994f45e7585f4b3096b48
LuizFerK <luizfernandoklein@live.com>
2022-01-04 02:55:05 -0300

    refactor: Subs server toward GenericServer

--- lib/servy/subs_server.ex
+++ lib/servy/subs_server.ex
@@ -1,69 +1,83 @@
-defmodule Servy.SubsServer do
-  @process_name :sub_server
-
-  # Client
-  def start(initial_state \\ []) do
-    IO.puts("Starting the subs server...")
-    pid = spawn(__MODULE__, :listen_loop, [initial_state])
-
-    Process.register(pid, @process_name)
-
+defmodule Servy.GenericServer do
+  def start(callback_module, initial_state, process_name) do
+    pid = spawn(__MODULE__, :listen_loop, [initial_state, callback_module])
+    Process.register(pid, process_name)
     pid
   end
 
-  def subscribe(name, amount) do
-    send(@process_name, {self(), :subscribe, name, amount})
+  def call(pid, message) do
+    send(pid, {:call, self(), message})
 
     receive do
-      {:status, status} -> status
+      {:response, response} -> response
     end
   end
 
-  def recent_subs do
-    send(@process_name, {self(), :recent_subs})
-
-    receive do
-      {:state, subs} -> subs
-    end
+  def cast(pid, message) do
+    send(pid, {:cast, message})
   end
 
-  def total_sub do
-    send(@process_name, {self(), :total_sub})
-
+  def listen_loop(state, callback_module) do
     receive do
-      {:total, total} -> total
-    end
-  end
+      {:call, pid, message} when is_pid(pid) ->
+        {response, state} = callback_module.handle_call(message, state)
+        send(pid, {:response, response})
+        listen_loop(state, callback_module)
 
-  # Server
-  def listen_loop(state) do
-    receive do
-      {pid, :subscribe, name, amount} ->
-        {:ok, id} = send_sub_to_service(name, amount)
-        state = [{name, amount} | Enum.take(state, 2)]
-
-        send(pid, {:status, id})
-
-        listen_loop(state)
-
-      {pid, :recent_subs} ->
-        send(pid, {:state, state})
-
-        listen_loop(state)
-
-      {pid, :total_sub} ->
-        total = Enum.map(state, &elem(&1, 1)) |> Enum.sum()
-        send(pid, {:total, total})
-
-        listen_loop(state)
+      {:cast, message} ->
+        state = callback_module.handle_cast(message, state)
+        listen_loop(state, callback_module)
 
       unexpected ->
         IO.puts("Unexpected messaged: #{inspect(unexpected)}")
-        listen_loop(state)
+        listen_loop(state, callback_module)
     end
   end
+end
 
-  defp send_sub_to_service(_, _) do
+defmodule Servy.SubsServer do
+  alias Servy.GenericServer
+
+  @process_name :sub_server
+
+  # Client
+  def start, do: GenericServer.start(__MODULE__, [], @process_name)
+
+  def subscribe(name, amount) do
+    GenericServer.call(@process_name, {:subscribe, name, amount})
+  end
+
+  def recent_subs do
+    GenericServer.call(@process_name, :recent_subs)
+  end
+
+  def total_sub do
+    GenericServer.call(@process_name, :total_sub)
+  end
+
+  def clear do
+    GenericServer.cast(@process_name, :clear)
+  end
+
+  # Server callbacks
+  def handle_call(:total_sub, state) do
+    total = Enum.map(state, &elem(&1, 1)) |> Enum.sum()
+
+    {total, state}
+  end
+
+  def handle_call(:recent_subs, state), do: {state, state}
+
+  def handle_call({:subscribe, name, amount}, state) do
+    {:ok, id} = send_sub_to_service(name, amount)
+    state = [{name, amount} | Enum.take(state, 2)]
+
+    {id, state}
+  end
+
+  def handle_cast(:clear, _state), do: []
+
+  def send_sub_to_service(_, _) do
     # simulation
     {:ok, "sub-#{:rand.uniform(1000)}"}
   end
